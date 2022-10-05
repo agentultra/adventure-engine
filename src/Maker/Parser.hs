5c9fc3275477289d5b9882a5953f18e053cf0a5c
James King <james@agentultra.com>
2022-10-04 21:33:56 -0400

    Add room exit parsing to the room parser
    We add a new `EntityReference` syntax for referring to entities:
    
      (1. Foo Bar)
    
    And this change updates the room definition to have a property list of
    entity references.  In this case, exits like so:
    
      -- Exits: (1. Foo Bar)
    
    Validation isn't handled here and will be addressed in a later change.

--- src/Maker/Parser.hs
+++ src/Maker/Parser.hs
@@ -47,15 +47,20 @@ roomParser = do
   lift . put $ s { seenRoomEntityIds = roomId `S.insert` seenRoomEntityIds }
   roomName <- nonEmptyLineParser
   roomDescLines <- manyTill lineParser $ single '#'
+  void newline
+  exitIds <- propertyParser @Exit "Exits"
   let room = Room
         { _roomName = roomName
         , _roomDescription = T.strip . T.concat $ roomDescLines
         , _roomObjects = mempty
-        , _roomExits = mempty
+        , _roomExits = M.fromList . map exitRefToMap $ exitIds
         , _roomDig = Left "You can't do that here.."
         , _roomBackground = Nothing
         }
   pure (roomId, room)
+  where
+    exitRefToMap :: EntityRef Exit -> (Text, EntityId Exit)
+    exitRefToMap (EntityRef entityId exitName) = (exitName, entityId)
 
 sectionParser :: Text -> Parser a -> Parser b -> Parser [a]
 sectionParser sectionName p sep = do
@@ -81,8 +86,44 @@ worldParser = do
     , _worldPlayerMessages = mempty
     }
 
+-- | Represents a reference to an entity in the game source text.
+--
+-- Written as @(1. FooName)@ in the source.  We get the type parameter
+-- from the context of the parse.  If we're parsing a list of "exits"
+-- then we know 'a' must be of type 'Exit'.
+data EntityRef a
+  = EntityRef
+  { _entityRefEntityId :: EntityId a
+  , _entityRefName     :: Text
+  }
+  deriving (Eq, Show)
+
+-- | Parse an entity reference in the source text.
+--
+-- Callers should determine type parameter of the EntityId.
+entityRefParser :: Parser (EntityRef a)
+entityRefParser = do
+  void . single $ '('
+  entityId <- entityIdParser
+  space1
+  entityName <- T.pack <$> manyTill latin1Char (single ')')
+  pure $ EntityRef entityId entityName
+
+propertyParser :: Text -> Parser [EntityRef a]
+propertyParser lbl = do
+  void . string $ "-- "
+  void . string $ lbl
+  void . single $ ':'
+  space1
+  sepEndBy entityRefParser (void . string $ ", ")
+
 runMakerParser :: Parser a -> FilePath -> Text -> Either (ParseErrorBundle Text Void) a
 runMakerParser p fpath
   = runIdentity
   . (`evalStateT` MakerParseState S.empty)
   . runParserT p fpath
+
+testMakerParser :: Show a => Parser a -> Text -> IO ()
+testMakerParser p txt = do
+  let result = runMakerParser p "test" txt
+  print result
